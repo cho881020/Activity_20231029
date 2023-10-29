bc90c0048541020e5b92cf49a1a621ca75b093b1
cho881020 <cho881020@gmail.com>
2023-10-29 09:37:18 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/wku/activity_20231029/MainActivity.kt
@@ -0,0 +1,17 @@
+package kr.ac.wku.activity_20231029
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kr.ac.wku.activity_20231029.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+    }
+}
